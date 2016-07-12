519b778c579749228206a2c1930745c644e3d460
Minghan Lin <cd>
2016-07-12 22:50:15 +0800

    first html draft

---
+++ script/game.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+var holey = ['#hole1', '#hole2', '#hole3', '#hole4', '#hole5', '#hole6', '#hole7', '#hole8', '#hole9'];
+
+// $(holey[0]).html('<img src="img/mole(2).png">');
